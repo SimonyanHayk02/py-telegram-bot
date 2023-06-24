16dcb323013929666467acd70eb5824cf6242ddb
SimonyanHayk02 <hayk.simonyan.2002@gmail.com>
2023-06-24 23:09:31 +0400

    Final bot

--- main/aiogram_bot.py
+++ main/aiogram_bot.py
@@ -1,6 +1,5 @@
 import asyncio
 from pathlib import Path
-import types
 import aiogram
 import openai
 from dotenv import load_dotenv
@@ -8,7 +7,8 @@ import os
 from aiogram.dispatcher.filters import Text
 from gtts import gTTS
 from aiogram.types import InputFile
-from aiogram.types import ReplyKeyboardRemove
+from aiogram.dispatcher import FSMContext
+
 
 
 load_dotenv()
@@ -21,88 +21,84 @@ bot = aiogram.Bot(token=bot_token)
                   
 dp = aiogram.Dispatcher(bot)
 
-
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: aiogram.types.Message):
-   await bot.send_message(message.from_user.id, f"Hello {message.from_user.first_name} Welcome to Story Generator Bot!")
-   kb = [
-        [aiogram.types.KeyboardButton(text="Boy")],
-        [aiogram.types.KeyboardButton(text="Girl")]
-    ]
-   keyboard = aiogram.types.ReplyKeyboardMarkup(
-        keyboard=kb,
-        resize_keyboard=True,
-        input_field_placeholder="Выберите способ подачи"
-    )
-   keyboard = aiogram.types.ReplyKeyboardMarkup(keyboard=kb)
-   await message.answer("Please tell for a boy or a girl to generate a fairy tale.?", reply_markup=keyboard)
+    await bot.send_message(message.from_user.id, f"Привет {message.from_user.first_name} Добро пожаловать в Story Generator Bot!")
+    await bot.send_message(message.from_user.id,"Пожалуйста напишите имя персонажа с которым вы бы хотели сгенерировать сказку !")
+    print(message.text)
+    
 
 
-
-@dp.message_handler(lambda message: message.text == "Boy")
-async def generate_story_for_man(message: aiogram.types.Message):
-    await bot.send_message(message.chat.id, "Thank you! Now please send me the name of the character you would like to generate a fairy tale with")
-    await generate_story(message, "Boy")
-
-
-@dp.message_handler(lambda message: message.text == "Girl")
-async def generate_story_for_woman(message: aiogram.types.Message):
-    await bot.send_message(message.chat.id, "Thank you! Now please send me the name of the character you would like to generate a fairy tale with")
-    await generate_story(message, "Girl")
+@dp.message_handler()
+async def set_character_name(message:aiogram.types.Message):
+    print(message.text)
+    if message.text != "Мальчик" and message.text != "Девочка":
+        global user_input_text 
+        user_input_text = message
+        kb = [
+            [
+                aiogram.types.KeyboardButton(text="Мальчик"),
+                aiogram.types.KeyboardButton(text="Девочка")
+            ],
+        ]
+        keyboard = aiogram.types.ReplyKeyboardMarkup(
+            keyboard=kb,
+            resize_keyboard=True,
+            input_field_placeholder="Выберите способ подачи"
+        )
+        await message.reply("Подскажите пожалуйста для мальчика или девочки сгенерировать сказку?", reply_markup=keyboard)
+    else:
+        if message.text == "Мальчик":
+            await generate_story(user_input_text, "Мальчик")
+        elif message.text =="Девочка":
+             await generate_story(user_input_text,"Девочка")
+        
 
 
 # Asynchronous function for generating a story
-async def generate_story(message, character):
+async def generate_story(message: aiogram.types.Message,gender):
+            await bot.send_message(message.chat.id, text="Пожалуйста подождите пока сказка генерируется...")
+            chat_response_final = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=f"""Я хочу, чтобы ты был моим создателем подсказок. Мне нужно, чтобы вы создали сказку для {gender.lower()}
+     с моим характером. Вы создаете истории для {gender.lower()} в возрасте от 4 до 12 лет. Но убедитесь, что оно имеет смысл и не слишком велико, чтобы вы могли отправить его прямо сейчас.
+     А теперь сгенерируйте сказку с персонажем {message}.""",
+            temperature=1,
+            max_tokens=2048,
+            top_p=1
+            )
     
-    await bot.send_message(message.chat.id, text="Wait, a fairy tale is being generated...")
-    chat_response_final = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=f"""I want you to be my prompt maker. I need you to generate a fairy tale for {character.lower()}
-with my own character. You generate stories for {character.lower()} from 4 to 12 years old. But make sure it makes sense and isn't too big so you can send it right away.
-And now generate a fairy tale with the {message.text} character.""",
-        temperature=1,
-        max_tokens=2048,
-        top_p=1
-    )
-    
-    print(message.text)
-    result_final = chat_response_final.choices[0].text
+            print(message.text)
+            result_final = chat_response_final.choices[0].text
 
-    responce_summary = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=f"""I want you to give me a summary of this text.{result_final}""",
-        temperature=1,
-        max_tokens=2048,
-        top_p=1
-    )
-    summary = responce_summary.choices[0].text
+            responce_summary = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=f"""Я хочу, чтобы вы кратко изложили этот текст.{result_final}""",
+                temperature=1,
+                max_tokens=2048,
+                top_p=1
+            )
+            summary = responce_summary.choices[0].text
 
-    audio_file_path = "generated_story.mp3"
-    tts = gTTS(summary)
-    tts.save(audio_file_path)
+            if summary:
+                audio_file_path = "generated_story.mp3"
+                tts = gTTS(summary,lang="ru")
+                tts.save(audio_file_path)
 
-    voice = InputFile(audio_file_path)
-    await bot.send_voice(message.from_user.id, voice, caption="Summary of this fairy tale!")
+                voice = InputFile(audio_file_path)
+                await bot.send_voice(message.from_user.id, voice, caption="Краткое содержание этой сказки!")
 
-    # Удаление временного файла
-    os.remove(audio_file_path)
+                # Delete the temporary audio file
+                os.remove(audio_file_path)
 
-    # Send the generated story to the chat
-    await bot.send_message(message.chat.id, f"Fairy Tale  {result_final}")
-
+                # Send the generated story to the chat
+                await bot.send_message(message.chat.id, f"Сказка: {result_final}")
+            else:
+                await bot.send_message(message.chat.id, "Извините, я не смог создать действительную сказку. Пожалуйста, попробуйте еще раз.")
     
 
-# Обработчик сообщений
-# @dp.message_handler()
-# async def handle_message(message: aiogram.types.Message):
-#     # Отправляем сообщение о начале ожидания
-    
-#     # Асинхронно генерируем сказку
-#     await generate_story(message,character)
 
-# Запуск бота и непрерывное получение сообщений
 async def main():
     await dp.start_polling()
-
 if __name__ == '__main__':
     asyncio.run(main())
