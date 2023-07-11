aae81114b876b133857a07a9131c45b0126c8899
SimonyanHayk02 <hayk.simonyan.2002@gmail.com>
2023-07-11 14:14:54 +0400

    final add deploying and some changes

--- qr_code/qr.py
+++ qr_code/qr.py
@@ -7,7 +7,7 @@ bot_token = "6134840576:AAE63UvD5GWK48Ls7S3afVbcJJC4jtS2snk"
 qr = qrcode.QRCode(version=1, box_size=10, border=5)
 
 # Формирование данных для QR-кода (ссылка на бота)
-bot_link = "https://t.me/Movie_Filter_Category_Bot"
+bot_link = "https://www.singulart.com/en/artist/aram-simonyan-34860?campaign_id=1717&fbclid=IwAR0VcUvFJaKW4D_lS6hxInCS_NKM5FJRdp4Ft7ZddlPB3dKrvTd361xxtC0_aem_Ae0L5ItDEv86OcGwGB_e-Ueiu1XReQib6_zX1xdESaux7RYLBmz2rxMQNAFNkjpAmII"
 qr.add_data(bot_link)
 qr.make(fit=True)
 
